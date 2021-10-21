c8a8a5ccd98df1a6ea8469142c065af41942c066
unknown <daria.mankevych.ik.2021>
2021-10-21 11:57:14 +0300

    Labor 4.7

---
+++ Laboratorna 4.7.cpp
@@ -0,0 +1,62 @@
+﻿// Laboratorna 4.7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Манбкевич Дар'я
+// Обчислення суми ряду Тейлора за допомогою ітераційних циклів та рекурентних співвідношень
+// Лабораторна робота 4.7
+// Варіант 10
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double Arth(double x);
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "eps(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		n = 0; // вираз залежить від умови завдання варіанту
+		a = x; // вираз залежить від умови завдання варіанту
+		S = 1;
+		do {
+			n++;
+			R = x * x * (2 * n - 1) / (2 * n + 1); // вираз залежить від умови завдання варіанту
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << Arth(x) << " |"
+			<< setw(10) << setprecision(5) << S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;   
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
